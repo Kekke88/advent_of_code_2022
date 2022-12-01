8f47dacd4ca1740e0dcb12b096ec3a4c4bcc62cf
Henric <hej@henricjohansson.se>
2022-12-01 16:25:04 +0100

    chore: added execution times and small optimisation

--- 01/01.py
+++ 01/01.py
@@ -1,20 +1,21 @@
 import os
 import sys
- 
+import time
+
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from lib.misc import is_integer
 from Elf import ElfInventory
 
 elves = list()
 
+start = time.time()
 with open('01.input') as f:
     elf = ElfInventory(total_calories=0)
 
     for line in f.readlines():
-        if is_integer(line):
+        if line != '\n':
             elf.add_food(int(line))
         else:
             elves.append(elf)
@@ -24,7 +25,9 @@ elves.append(elf)
 elves.sort()
 
 part_one = elves.pop().total_calories
-part_two = part_one + elves.pop().total_calories + elves.pop().total_calories
+part_one_time_in_ms = (time.time() - start) * 1000
+print(f"Part 1: {part_one}, execution time: {part_one_time_in_ms}ms")
 
-print(f"Part 1: {part_one}")
-print(f"Part 2: {part_two}")
+part_two = part_one + elves.pop().total_calories + elves.pop().total_calories
+part_two_time_in_ms = (time.time() - start) * 1000
+print(f"Part 2: {part_two}, execution time: {part_two_time_in_ms}ms")
