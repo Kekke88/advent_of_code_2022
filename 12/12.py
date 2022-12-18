acd4a599b1abcb5cc7ee19b0a52db461d5f6569d
Henric Johansson <henric-johansson@hotmail.com>
2022-12-18 21:47:52 +0100

    feat: day 12 solutions

--- 12/12.py
+++ 12/12.py
@@ -9,51 +9,18 @@ sys.path.append(parent)
 from lib.Vector2d import Vector2d
 
 height_map = list()
+visited = []
 
-
-def is_available(from_pos: Vector2d, to_pos: Vector2d, visited: dict) -> bool:
-    print(
-        f"Checking {height_map[from_pos.y][from_pos.x]} -> {height_map[to_pos.y][to_pos.x]} ({ord(height_map[from_pos.y][from_pos.x])} -> {ord(height_map[to_pos.y][to_pos.x])})"
-    )
-
-    check_char = height_map[to_pos.y][to_pos.x]
-    if check_char == "E":
-        check_char = "z"
-
-    return (
-        ord(height_map[from_pos.y][from_pos.x]) + 1 >= ord(check_char) + 1
-        and not f"{to_pos.x}:{to_pos.y}" in visited
-    )
-
-
-def find_start_pos(height_map: list) -> Vector2d:
-    search_pos = Vector2d()
-
-    for i in range(0, len(height_map)):
-        found = height_map[i].find("S")
-        if found != -1:
-            search_pos.x = found
-            search_pos.y = i
-
-    return search_pos
-
-
-def find_paths(height_map: list, visited: dict, position: Vector2d) -> dict:
-    print("--- VISITED ---")
-    print(visited)
-    print(f"Current position: {height_map[position.y][position.x]}")
-    if height_map[position.y][position.x] == "E":
-        print("Returning")
-        return visited
+def get_available_nodes(height_map: list, position: Vector2d):
     test_pos = Vector2d()
+    return_nodes = list()
     # North
     test_pos.x = position.x
     test_pos.y = position.y - 1
     if position.y > 0 and is_available(
         from_pos=position, to_pos=test_pos, visited=visited
     ):
-        visited[f"{position.x}:{position.y - 1}"] = True
-        find_paths(height_map=height_map, visited=visited, position=test_pos)
+        return_nodes.append(Vector2d(test_pos.x, test_pos.y))
 
     # East
     test_pos.x = position.x + 1
@@ -61,8 +28,7 @@ def find_paths(height_map: list, visited: dict, position: Vector2d) -> dict:
     if len(height_map[test_pos.y]) > test_pos.x and is_available(
         from_pos=position, to_pos=test_pos, visited=visited
     ):
-        visited[f"{position.x + 1}:{position.y}"] = True
-        find_paths(height_map=height_map, visited=visited, position=test_pos)
+        return_nodes.append(Vector2d(test_pos.x, test_pos.y))
 
     # South
     test_pos.x = position.x
@@ -70,8 +36,7 @@ def find_paths(height_map: list, visited: dict, position: Vector2d) -> dict:
     if len(height_map) > test_pos.y and is_available(
         from_pos=position, to_pos=test_pos, visited=visited
     ):
-        visited[f"{position.x}:{position.y + 1}"] = True
-        find_paths(height_map=height_map, visited=visited, position=test_pos)
+        return_nodes.append(Vector2d(test_pos.x, test_pos.y))
 
     # West
     test_pos.x = position.x - 1
@@ -79,25 +44,91 @@ def find_paths(height_map: list, visited: dict, position: Vector2d) -> dict:
     if test_pos.x > 0 and is_available(
         from_pos=position, to_pos=test_pos, visited=visited
     ):
-        visited[f"{position.x + 1}:{position.y}"] = True
-        find_paths(height_map=height_map, visited=visited, position=test_pos)
+        return_nodes.append(Vector2d(test_pos.x, test_pos.y))
 
+    return return_nodes
+
+
+def shortest_path(height_map: list, position: Vector2d, end: Vector2d):
+    path_list = [[position]]
+    path_index = 0
+
+    previous_nodes = {f"{position.x}:{position.y}"}
+    if position == end:
+        return path_list[0]
+        
+    while path_index < len(path_list):
+        current_path = path_list[path_index]
+        last_node = current_path[-1]
+        next_nodes = get_available_nodes(height_map, last_node)
+
+        if end in next_nodes:
+            current_path.append(end)
+            return current_path
+
+        for next_node in next_nodes:
+            if not f"{next_node.x}:{next_node.y}" in previous_nodes:
+                new_path = current_path[:]
+                new_path.append(next_node)
+                path_list.append(new_path)
+                previous_nodes.add(f"{next_node.x}:{next_node.y}")
+        path_index += 1
+    return []
+
+
+def is_available(from_pos: Vector2d, to_pos: Vector2d, visited: dict) -> bool:
+    check_char = height_map[to_pos.y][to_pos.x]
+    if check_char == "E":
+        check_char = "z"
+
+    return ord(height_map[from_pos.y][from_pos.x]) + 1 >= ord(check_char)
+
+
+def find_pos(height_map: list, character: str) -> Vector2d:
+    search_pos = Vector2d()
+
+    for i in range(0, len(height_map)):
+        found = height_map[i].find(character)
+        if found != -1:
+            search_pos.x = found
+            search_pos.y = i
+
+    return search_pos
+
+def find_start_positions(height_map: list) -> list:
+    start_positions = list()
+
+    for i in range(0, len(height_map)):
+        for j in range(0, len(height_map[i])):
+            if height_map[i][j] == "a":
+                start_positions.append(Vector2d(j, i))
+
+    return start_positions
 
 start = time.time()
-with open("12.example.input") as f:
+with open("12.input") as f:
     for line in f.readlines():
         height_map.append(line.replace("\n", ""))
 
-start_pos = find_start_pos(height_map=height_map)
-print(f"Start pos: {start_pos.x}:{start_pos.y}")
+start_pos = find_pos(height_map=height_map, character="S")
+end_pos = find_pos(height_map=height_map, character="E")
 height_map[start_pos.y] = height_map[start_pos.y].replace("S", "a")
-print(height_map)
-find_paths(height_map=height_map, visited={f"{start_pos.x}:{start_pos.y}": True}, position=start_pos)
 
-part_one = "WIP"
+solution = shortest_path(height_map=height_map, position=start_pos, end=end_pos)
+
+part_one = len(solution) - 1
 print(f"Part 1: {part_one}")
 
-part_two = "WIP"
+solutions = list()
+start_positions = find_start_positions(height_map=height_map)
+for start_pos in start_positions:
+    path_length = len(shortest_path(height_map=height_map, position=start_pos, end=end_pos))
+    if path_length > 0: 
+        solutions.append(path_length)
+
+solutions.sort()
+
+part_two = solutions[0] - 1
 print(f"Part 2: {part_two}")
 
 print(f"Total execution time: {(time.time() - start) * 1000} ms")
